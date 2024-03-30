a081893d3472d3b0ae64de4513dbf67120d9c57e
pouyasadri <pouyapaghandeh78@gmail.com>
2024-03-30 16:10:21 +0100

    Prepare the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Pouya sadri',
+            'email' => 'pouya@example.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => time(),
         ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
     }
 }
